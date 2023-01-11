e7f08ca933a9154d9410ad7767dfe5e093876cd8
Evgeny Fisenko <evgeny.f.aircelle@gmail.com>
2023-01-11 15:56:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello, World! My name is Java.";
+        String[] arr = str.split(" ");
+        for (String s : arr) {
+            System.out.println(s);
+
+
+        }
+    }
+}
+
